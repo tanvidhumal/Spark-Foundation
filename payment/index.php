97fa6e65f8cd367454e631b869ee9e4d3fb3d48d
tanvidhumal <75524873+tanvidhumal@users.noreply.github.com>
2021-06-18 22:18:20 +0530

    Add files via upload

---
+++ payment/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Spark Foundation</title>
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu" rel="stylesheet">
+
+  <!-- CSS Stylesheets -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Font Awesome -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+  
+
+ 
+</head>
+
+<body>
+
+  <section class="colored-section" id="title">
+
+  
+
+      <!-- Nav Bar -->
+      <div class="container-nav">
+      <nav class="navbar navbar-expand-lg navbar-dark" id="navb">
+
+        <a class="navbar-brand" href="">feedIndia</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+          <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+              <a class="nav-link" href="#title">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#features">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#missions">Our Mission</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#donate">Donate</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#footer">Conatact</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+</div>
+
+      <!-- Title -->
+     
+      <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-lg-6">
+          <h1 class="big-headings">Some waste food to show that they are Rich,<br>Few donate food because they have a Rich Heart</h1>
+          <h6 class="head">Because #FoodHeals<h6>
+          <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HOV3G7c2fahecX" async> </script> </form>
+        </div>
+
+        <div class="col-lg-6 ">
+          <img  class="title-image" src="img/donation.jpg" alt="iphone-mockup">
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+
+  <!-- Features -->
+
+  <section class="white-section" id="features">
+
+    <div class="container-fluid">
+
+      <div class="row">
+        <div class="feature-box col-lg-4">
+          <i class="icon fas fa-users fa-4x"></i>
+          <h3 class="feature-title">Volunteers</h3>
+          <p>Feeding India by network of volunteers (aka Hunger Heroes) is the backbone of the organisation.</p>
+        </div>
+
+        <div class="feature-box col-lg-4">
+          <i class="icon fas fa-handshake fa-4x"></i>
+          <h3 class="feature-title">Partners</h3>
+          <p>Our corporate partner initiatives help us amplify our impact and spread the word about the cause.</p>
+        </div>
+
+        <div class="feature-box col-lg-4">
+        <i class="icon fas fa-heart fa-4x"></i>
+          <h3 class="feature-title">NGO's</h3>
+          <p>Our NGO partners help us identify and support the right beneficiaries.</p>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+
+
+  <!-- About -->
+
+  <section class="colored-section" id="about">
+  <div class="container-fluid">
+  <h4 class="big-heading">About US</h4>
+  <h4>“Hunger is not a problem. It is an obscenity. How wonderful it is that nobody need wait a single moment before starting to improve the world.” – Anne Frank</h4>
+<hr>
+<h5 class="about-us">No Food Waste is an organisation which aims to redistribute excess food from weddings, parties, events to those who are hungry. 
+
+The idea took root in Coimbatore, where we started with 2 Shopper Bags and a volunteer to collect the surplus food and deliver to the homeless through Public Transportation System from October 16th, 2014. We took an oath on that day to stop food waste and satisfy the hunger of many, in a quest to make the “World Hunger Free.”
+
+We have picked and distributed food 96% times, provided food to a million hungry people, saved nearly  321 tonnes of food, which amounts to nearly Rs 4.87 crores. 
+
+No Food Waste works alongside Governments, Corporates with Social Responsibility on their minds, other partners and Volunteers to create a local chapter of excess food collection institution, which will be spread across various cities, addressing the two major challenges – namely urban and environmental, following the United Nations Sustainable Development Goals.
+
+We aim to bring benefits across the spectrum of society and help provide access to food to those who don’t have adequate means to access it.</h5>
+<hr>
+</div>
+
+    <h3 class="big-heading">Our Trusted Partners</h3>
+    <img class="press-logo" src="img/1.webp" alt="tc-logo">
+    <img class="press-logo" src="img/2.webp" alt="tnw-logo">
+    <img class="press-logo" src="img/3.webp" alt="biz-insider-logo">
+    <img class="press-logo" src="img/4.webp" alt="mashable-logo">
+
+  </section>
+
+
+
+  <!-- Our Mission-->
+
+  <section class="white-section" id="missions">
+
+    <div class="container-fluid">
+    <h3 class="big-heading">
+      Our Mission</h3>
+      <h5 class="mission">=>We bring holistic food and nutrition to underprivileged children</h5>
+      <h5 class="mission">=>To feed the needy and hungry with untouchable edible surplus food.</h5>
+      <h5 class="mission">=>To sensitize people about the amount of food being wasted through standardized food waste auditing methods.</h5>
+      <h5 class="mission">=>To provide alternative solutions for spoiled or wasted food ensuring that too not reaches the landfills, dumpsites or dustbins.</h5>
+      <h5 class="mission">=>To raise awareness about food loss and food waste amongst citizens and bring about behavioral change in them to prevent food waste at home, school or at workplace through guidance and sharing of good practices</h5>
+      <h5 class="mission">=>To raise awareness among Food businesses by encouraging them to adopt good practices to reduce food loss and food waste in their supply chains and sharing good practices.</h5>
+      <h5 class="mission">=>This makes Spark Foundation the only non-profit organisation providing this service in the country</h5><br><br>
+      
+    </div>
+  </section>
+
+  
+  <!-- Donation-->
+
+  <section class="colored-section" id="donate">
+
+    <div class="container-fluid">
+        <img class="image" src="img/bit.jpg" alt="tnw-logo">
+        <h4 class="info">“I have seen that hunger can drive people to extremes and even get into unlawful activities. If we take care of their basic necessity of a wholesome meal, we may just give them a chance to shape a better future for themselves.”<br>
+        Mr. D Sivanandhan
+        Former Police Commissioner Mumbai</h4>
+        <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HOV3G7c2fahecX" async> </script> </form>
+    </div>
+  </section>
+
+
+
+
+  <!-- Footer -->
+
+  <footer class="white-section" id="footer">
+    <div class="container-fluid">
+   
+      <h3 class="big-heading">Reach out, even if it is for a chat.</h3>
+      <h3 class="con">Office address:</h3> 
+      <h5 class="con">407, 4th Floor, abcd, <br>
+        156, xyz Rd, 
+        xyz, 
+        pqr 400134 
+        022-49678912</h5>
+      <a href="https://www.facebook.com/"><i class="social-icon fab fa-facebook-f"></i></a>
+     <a href="https://twitter.com/?lang=en"> <i class="social-icon fab fa-twitter"></i></a>
+      <a href="https://www.instagram.com/thesparksfoundation.info?utm_medium=copy_link"><i class="social-icon fab fa-instagram"></i></a>
+      <a href="https://www.google.com/gmail/about/"><i class="social-icon fas fa-envelope"></i></a>
+     
+    </div>
+  </footer>
+
+</body>
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
